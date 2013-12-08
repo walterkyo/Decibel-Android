fa80eb1bece62b951a4f419d1932d50f0542b61f
Bruna Seewald <brunaseewald@Brunas-MacBook-Pro.local>
2013-12-08 11:25:21 -0200

    Interface do aplicativo

--- src/com/embarcados/activity/AcquirementActivity.java
+++ src/com/embarcados/activity/AcquirementActivity.java
@@ -3,6 +3,17 @@
  */
 package com.embarcados.activity;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.TimeSeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
 import com.embarcados.bluetooth.BluetoothReceiverFactory;
 import com.embarcados.bluetooth.IBluetoothReceiveListener;
 import com.embarcados.bluetooth.IBluetoothReceiver;
@@ -10,7 +21,13 @@ import com.embarcados.decibel_android.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint.Align;
 
 /**
  * @author Mauricio L. Dau <mauricioldau@gmail.com>
@@ -21,6 +38,12 @@ public class AcquirementActivity extends Activity implements IBluetoothReceiveLi
 	private final IBluetoothReceiver bluetoothReceiver;
 	
 	private TextView valueTextView;
+	private TimeSeries dataset;
+	private XYMultipleSeriesDataset mDataset;
+	private XYSeriesRenderer renderer;
+	private XYMultipleSeriesRenderer mRenderer;
+	private GraphicalView mChartView;
+	Calendar calendar = new GregorianCalendar();
 	
 	public AcquirementActivity() {
 		
@@ -36,9 +59,33 @@ public class AcquirementActivity extends Activity implements IBluetoothReceiveLi
 
 		super.onCreate(savedInstanceState);
 		
-		setContentView(R.layout.acquirement_layout);
-		
+		setContentView(R.layout.acquirement_layout);	
 		this.valueTextView = (TextView) this.findViewById(R.id.textView1);
+
+		//Grafico dinamico
+		dataset = new TimeSeries("Valores");
+		mDataset = new XYMultipleSeriesDataset();
+		
+		renderer = new XYSeriesRenderer();
+		mRenderer = new XYMultipleSeriesRenderer();
+		
+		mDataset.addSeries(dataset);
+		
+		renderer.setColor(Color.GREEN);
+		renderer.setPointStyle(PointStyle.CIRCLE);
+		renderer.setFillPoints(true);
+		
+		mRenderer.setZoomButtonsVisible(true);
+		mRenderer.setXTitle("Segundos");
+		mRenderer.setYTitle("Decibeis");
+		mRenderer.setAxisTitleTextSize(20);
+	    //mRenderer.setYAxisMin(30);
+	    //mRenderer.setYAxisMax(80);
+		mRenderer.setShowGrid(true);
+		mRenderer.setGridColor(Color.DKGRAY);
+		
+		mRenderer.addSeriesRenderer(renderer);
+		
 	}
 
 	@Override
@@ -47,6 +94,15 @@ public class AcquirementActivity extends Activity implements IBluetoothReceiveLi
 		super.onStart();
 		
 		this.bluetoothReceiver.start();
+		
+		if (mChartView == null) {
+		    LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
+		    mChartView = ChartFactory.getLineChartView(this, mDataset, mRenderer);
+		    layout.addView(mChartView, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+		  } else {
+		    mChartView.repaint();
+		}
+		
 	}
 	
 	@Override
@@ -67,7 +123,9 @@ public class AcquirementActivity extends Activity implements IBluetoothReceiveLi
 	
 	@Override
 	public void onNewValue(int newValue) {
-
+		
+		dataset.add(calendar.get(Calendar.SECOND),newValue);
+		mChartView.repaint();
 		this.valueTextView.setText(String.valueOf(newValue));
 	}
 	
